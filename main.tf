72514ae7fe751325ced027853548013410db9d3c
Yash <yashdixitlegendary@gmail.com>
2025-04-06 15:38:49 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "a98d0e19-0c0e-4a5b-91ac-c6923c6331bc"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-jenkins"
+  location = "East US"
+}
+
+# Create App Service Plan
+resource "azurerm_service_plan" "asp" {
+  name                = "jenkins-app-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku_name = "S1"
+  os_type  = "Linux"
+}
+
+# Create App Service for Jenkins
+resource "azurerm_linux_web_app" "jenkins" {
+  name                = "jenkins-yd-webapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = true
+
+    application_stack {
+      docker_image_name = "jenkins/jenkins:lts"
+    }
+  }
+}
+
+
